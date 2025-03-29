c764811c72d3c2148f17fcd1ac43c52e9713975c
Chubie Mirembe - Windows <mirembechubie@googlemail.com>
2025-03-29 13:32:44 +0000

    exit button test_1

--- src/main.cpp
+++ src/main.cpp
@@ -10,8 +10,12 @@ int main() {
     std::cout << "$ ";
 
     std::string input;
-    std::getline(std::cin, input);
-    std::cout << input << ": command not found" << std::endl;
+    if(input == "exit 0"){
+      break;
+    } else {
+      std::getline(std::cin, input);
+      std::cout << input << ": command not found" << std::endl;
+    }
   }
   
 }
