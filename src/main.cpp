7fe952d3820d0545a0bade3e469d32f91770c264
Chubie Mirembe - Windows <mirembechubie@googlemail.com>
2025-03-29 14:14:05 +0000

    bulitin test_1

--- src/main.cpp
+++ src/main.cpp
@@ -18,6 +18,14 @@ int main() {
       std::string str = input.substr(pos+1);
       std::cout << str << std::endl;
 
+    } else if(input.find("type") != std::string::npos){
+      int pos = input.find(" ");
+      std::string str = input.substr(pos+1);
+      if(str == "echo" || str == "exit" || str == "type"){
+        std::cout << str << " is a shell builtin" << std::endl;
+      } else {
+        std::cout << str << ": not found" << std:endl;
+      }
     } else {
       std::cout << input << ": command not found" << std::endl;
     }
